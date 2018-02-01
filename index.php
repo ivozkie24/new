0746320620b8b0f2f96a2f7614f2bffeb0dd83ec
ivozkie24 <35474923+ivozkie24@users.noreply.github.com>
2018-02-01 11:30:00 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Jessie's Cisco</title>
+</head>
+<body>
+
+<label>Cisco is life</label>
+<tr>
+
+	<td><img src="img/router.png"></td>
+	<p>thisrouter is so good</p>
+
+</tr>
+<td><img src="img/hub.jpg"></td>
+<p>this hub is so bad</p>
+<tr>
+	
+
+
+</body>
+</html>
